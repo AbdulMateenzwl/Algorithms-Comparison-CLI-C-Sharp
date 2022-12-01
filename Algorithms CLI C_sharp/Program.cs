dcac971cc87cacb04b6aaccadcb2bb7b3d4549e0
Abdul Mateen <98536971+notrealmateen@users.noreply.github.com>
2022-12-01 10:03:50 +0500

    Add project files.

---
+++ Algorithms CLI C_sharp/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms_CLI_C_sharp.BL;
+using Algorithms_CLI_C_sharp.DL;
+using Algorithms_CLI_C_sharp.UI;
+namespace Algorithms_CLI_C_sharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			List<string> filenames = new List<string>() { "", "organizations-100.csv", "organizations-1000.csv", "organizations-10000.csv", "organizations-100000.csv", "organizations-500000.csv", "temp.csv" };
+			List<string> sortType = new List<string>() { "", "Selection Sort ", "Bubble Sort ", "Insertion Sort ", "Merge Sort ", "Quick Sort", "Heap Sort", "Counting Sort", "Radix Sort", "Bucket Sort" };
+			List<string> sortBy = new List<string>() { "", "by Index", "by No of Employees" };
+			Data organizations = new Data();
+			organizations.set_Data(OrganizationDl.load_data(filenames[6]));
+			while (true)
+			{
+				char ch = OrganizationUI.mianMenu();
+				if (ch == '1') // Sort by Index
+				{
+					char ch2 = OrganizationUI.sortDataMenu();
+					if (ch2 == '0')
+					{
+						continue;
+					}
+					else
+					{
+						var watch = new System.Diagnostics.Stopwatch();
+						watch.Start();
+						List<Org> vtr = organizations.sortData(ch2-48, ch-48, organizations.get_Data());
+						watch.Stop();
+						OrganizationUI.showtime(watch.ElapsedMilliseconds, sortType[ch2 - 48] + sortBy[ch - 48]);
+						OrganizationDl.storetime(watch.ElapsedMilliseconds, sortType[ch2 - 48] + sortBy[ch - 48]);
+						organizations.set_Data(vtr);
+					}
+				}
+				else if (ch == '2') // Sort by no of employees
+				{
+					char ch2 = OrganizationUI.sortDataMenu();
+					if (ch2 == '0')
+					{
+						continue;
+					}
+					else
+					{
+						var watch = new System.Diagnostics.Stopwatch();
+						watch.Start();
+						List<Org> vtr = organizations.sortData(ch2-48, ch-48, organizations.get_Data());
+						watch.Stop();
+						OrganizationUI.showtime(watch.ElapsedMilliseconds, sortType[ch2 - 48] + sortBy[ch - 48]);
+						OrganizationDl.storetime(watch.ElapsedMilliseconds, sortType[ch2 - 48] + sortBy[ch - 48]);
+						organizations.set_Data(vtr);
+					}
+				}
+				else if (ch == '3') // Load Data from file
+				{
+					int op = OrganizationUI.loadDataMenu();
+					if (op == 7)
+					{
+						continue;
+					}
+					var watch = new System.Diagnostics.Stopwatch();
+					watch.Start();
+					List<Org> vtr = OrganizationDl.load_data(filenames[op]);
+					watch.Stop();
+					OrganizationUI.showtime(watch.ElapsedMilliseconds, "Load Data " + filenames[op]);
+					OrganizationDl.storetime(watch.ElapsedMilliseconds, "Load Data " + filenames[op]);
+					organizations.set_Data(vtr);
+				}
+				else if (ch == '4') // Display All Data
+				{
+					OrganizationUI.displayAllData(organizations.get_Data());
+				}
+				else if (ch == '5') // Display Indexes
+				{
+					OrganizationUI.displayIndexesData(organizations.get_Data());
+				}
+				else if (ch == '6') // Display No of Employees
+				{
+					OrganizationUI.displayNoEmpData(organizations.get_Data());
+				}
+				else if (ch == '7') // StoreData
+				{
+					OrganizationDl.storeData(organizations.get_Data());
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+    }
+}
---
+++ Algorithms CLI C_sharp/UI/OrganizationUI.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms_CLI_C_sharp.BL;
+namespace Algorithms_CLI_C_sharp.UI
+{
+    public class OrganizationUI
+    {
+        public static void displayAllData(List<Org> vtr)
+        {
+            Console.Write("{0,-5}", "Index");
+            Console.Write("{0,-20}", "Organization ID");
+            Console.Write("{0,-35}", "Name");
+            Console.Write("{0,-40}", "Name");
+            Console.Write("{0,-45}", "Country");
+            Console.Write("{0,-60}", "Discription");
+            Console.Write("{0,-10}", "Founded");
+            Console.Write("{0,-60}", "Industry");
+            Console.Write("{0,-30}", "No. of Employees");
+            Console.Write("{0,-30}", "\n");
+            foreach (var record in vtr)
+            {
+                Console.Write("{0,-5}", record.index);
+                Console.Write("{0,-20}", record.org_id);
+                Console.Write("{0,-35}", record.name);
+                Console.Write("{0,-40}", record.website);
+                Console.Write("{0,-45}", record.country);
+                Console.Write("{0,-60}", record.discription);
+                Console.Write("{0,-10}", record.founded);
+                Console.Write("{0,-60}", record.industry);
+                Console.Write("{0,-30}", record.no_emp);
+                Console.Write("{0,-30}", "\n");
+            }
+        }
+        public static void displayNoEmpData(List<Org> vtr)
+        {
+            foreach (var record in vtr)
+            {
+                Console.WriteLine(record.no_emp);
+            }
+        }
+        public static void displayIndexesData(List<Org> vtr)
+        {
+            foreach (var record in vtr)
+            {
+                Console.WriteLine(record.index);
+            }
+        }
+        public static char mianMenu()
+        {
+            char ch;
+            Console.Write("\n");
+            Console.Write("1. Sort with Index");
+            Console.Write("\n");
+            Console.Write("2. Sort with No of Employees");
+            Console.Write("\n");
+            Console.Write("3. Load Data");
+            Console.Write("\n");
+            Console.Write("4. Display All Data");
+            Console.Write("\n");
+            Console.Write("5. Display Indexes");
+            Console.Write("\n");
+            Console.Write("6. Dispay No of Employees");
+            Console.Write("\n");
+            Console.Write("7. Store Data");
+            Console.Write("\n");
+            Console.Write("0. Exit");
+            Console.Write("\n");
+            ch = Console.ReadLine()[0];
+            return ch;
+        }
+        public static int loadDataMenu()
+        {
+            int ch;
+            Console.Write("1. Load 100 Records");
+            Console.Write("\n");
+            Console.Write("2. Load 1000 Records");
+            Console.Write("\n");
+            Console.Write("3. Load 10000 Records");
+            Console.Write("\n");
+            Console.Write("4. Load 100000 Records");
+            Console.Write("\n");
+            Console.Write("5. Load 500000 Records");
+            Console.Write("\n");
+            Console.Write("6. Load Previous Stored Data");
+            Console.Write("\n");
+            Console.Write("0. Back");
+            Console.Write("\n");
+            ch = int.Parse(Console.ReadLine());
+            return ch;
+        }
+        public static char sortDataMenu()
+        {
+            char ch;
+            Console.Write("1. Selection Sort");
+            Console.Write("\n");
+            Console.Write("2. Bubble Sort");
+            Console.Write("\n");
+            Console.Write("3. Insertion Sort");
+            Console.Write("\n");
+            Console.Write("4. Merge Sort");
+            Console.Write("\n");
+            Console.Write("5. Quick Sort");
+            Console.Write("\n");
+            Console.Write("6. Heap Sort");
+            Console.Write("\n");
+            Console.Write("7. Count Sort");
+            Console.Write("\n");
+            Console.Write("8. Radix Sort");
+            Console.Write("\n");
+            Console.Write("9. Bucket Sort");
+            Console.Write("\n");
+            Console.Write("0. Back");
+            Console.Write("\n");
+            ch = Console.ReadLine()[0];
+            return ch;
+        }
+        public static char sortOptions()
+        {
+            char ch;
+            Console.Write("1. Accending Sort");
+            Console.Write("\n");
+            Console.Write("2. Descending Sort");
+            Console.Write("\n");
+            Console.Write("3. Back");
+            Console.Write("\n");
+            ch = Console.ReadLine()[0];
+            return ch;
+        }
+        public static void showtime(long time, string str)
+        {
+            Console.WriteLine("Time Taken for " + str + " : " + time + " ms ");
+        }
+    }
+}
